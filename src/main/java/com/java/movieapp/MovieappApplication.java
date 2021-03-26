ab91e8d97710da00bbfd6e282278e1c36e6f75cb
nguyen-son762 <62460681+nguyen-son762@users.noreply.github.com>
2021-03-26 21:10:55 +0700

    fix

--- src/main/java/com/java/movieapp/MovieappApplication.java
+++ src/main/java/com/java/movieapp/MovieappApplication.java
@@ -2,9 +2,11 @@ package com.java.movieapp;
 
 import com.java.movieapp.filter.AuthFilter;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
