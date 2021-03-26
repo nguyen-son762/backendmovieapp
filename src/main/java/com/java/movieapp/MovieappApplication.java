6efbc2c70f37a36528f82042d045f385f1cba9d7
nguyen-son762 <62460681+nguyen-son762@users.noreply.github.com>
2021-03-26 20:53:18 +0700

    fix

--- src/main/java/com/java/movieapp/MovieappApplication.java
+++ src/main/java/com/java/movieapp/MovieappApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -14,7 +12,6 @@ import org.springframework.web.filter.CorsFilter;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.java.movieapp.controllers"})
 public class MovieappApplication {
     public static void main(String[] args) {
         SpringApplication.run(MovieappApplication.class, args);
